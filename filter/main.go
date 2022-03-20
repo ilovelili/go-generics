be5bff84a0b13c184668acd96243c696ae4f1056
ilovelili <route666@live.cn>
2022-03-20 15:17:34 +0900

    filter

---
+++ filter/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func filter[E any](arr []E, f func(E) bool) []E {
+	result := []E{}
+	for _, item := range arr {
+		if f(item) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func main() {
+	a := []int{1, 2, 3}
+	b := filter(a, func(i int) bool {
+		return i < 2
+	})
+
+	fmt.Println(b)
+}
